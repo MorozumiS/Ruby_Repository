1f875ae739e50e63bf9d07be0a592ed5fab28bc4
両角星南 <morozumisena@sena.local>
2023-11-08 15:50:58 +0900

    name完全一致検索

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :update]
+  before_action :set_project, only: [:show, :update, :destroy]
 
   def create
     project = Project.new(project_params)
@@ -18,24 +18,47 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def update
-    if @project.nil?
-      render json: { error: "イベントが存在しません" }, status: :not_found
+  def show_by_name
+    @project = Project.find_by(name: params[:name])
+    if @project
+      render json: @project, status: :ok
     else
-      if @project.update(project_params)
-        render json: @project, status: :ok
-      else
-        render json: { error: "イベントの更新に失敗しました" }, status: :unprocessable_entity
-      end
+      render json: { error: "イベントが存在しません" }, status: :not_found
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      render json: @project, status: :ok
+    else
+      render json: { error: "イベントの更新に失敗しました" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @project.update(discarded_at: Time.current)
+      render json: project_response(@project), status: :ok
+    else
+      render json: { error: "イベントの削除に失敗しました" }, status: :unprocessable_entity
     end
   end
 
   private
   def project_params
-    params.permit(:name, :start_at, :end_at, :place, :client_id, :created_at, :updated_at)
+    params.permit(:name, :start_at, :end_at, :place, :user_id, :created_at, :updated_at)
   end
 
   def set_project
     @project = Project.find_by(id: params[:id])
+    if @project.nil?
+      render json: { error: "イベントが存在しません" }, status: :not_found
+    end
+  end
+
+  def project_response(project)
+    {name: project.name,
+    user_id: project.user_id,
+    created_at: project.created_at,
+    updated_at: project.updated_at}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:create, :show ,:update]
+  resources :projects, only: [:create, :show ,:update, :destroy] do
+    get 'name/:name', to: 'projects#show_by_name', on: :collection
+  end
 end
