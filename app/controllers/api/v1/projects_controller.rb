2a0de1bf71137903848f8a24f79727f861409d02
morozumisena <morozumi.sena@ring-tech.jp>
2024-01-09 14:24:57 +0900

    TODO指摘点修正

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,3 @@
-# TODO: 不要なコメントは削除してください
 class Api::V1::ProjectsController < ApplicationController
   before_action :set_project, only: %i[show update destroy]
 
@@ -10,52 +9,38 @@ class Api::V1::ProjectsController < ApplicationController
 
   # GET /api/v1/projects/:id
   def show
-    # TODO: ソースコードをリファクタリングしてください
     return unless @project&.delete_flg == false
-
     response_success(@project)
   end
 
   # GET /api/v1/projects/search
   def search
-    # TODO: エラー文が複数ある時は、複数返却できるようにして下さい
-    if params[:name].blank? && params[:place].blank?
-      error_message = I18n.t('response.message.blank_keywords')
-      return response_custom_error(error_message, :bad_request)
-    end
+    error_messages = []
 
-    if params[:name].present? && params[:name].length < 3
-      error_message = I18n.t('response.message.name_too_short')
-      return response_custom_error(error_message, :bad_request)
-    end
+    error_messages = I18n.t('response.message.blank_keywords') if params[:name].blank? && params[:place].blank?
 
-    if params[:place].present? && params[:place].length < 3
-      error_message = I18n.t('response.message.place_too_short')
-      return response_custom_error(error_message, :bad_request)
-    end
+    error_messages = I18n.t('response.message.name_too_short') if params[:name].present? && params[:name].length < 3
 
-    # TODO: 部分一致は「%」を左右につけて下さい
-    # TODO: ここ微妙なので、例を参考にしてリファクタリングして下さい
-    # [例：]
+    error_messages = I18n.t('response.message.place_too_short') if params[:place].present? && params[:place].length < 3
+
+    return response_custom_error(error_messages, :bad_request) if error_messages.any?
+
     name_query = ''
     name_query = params[:name] + '%' if params[:name].present?
-    # name_query = params[:name].present? ?  params[:name] + "%" : nil
-    place_query = params[:place].present? ? params[:place] + '%' : nil
 
-    # TODO: SQLインジェクション対策のため、where句には?を使って下さい(プレスホルダーの使用)
-    # 参考: https://pikawaka.com/rails/where
-    if name_query && place_query
-      projects = Project.where("name LIKE ? AND place LIKE ?", name_query, place_query)
-    else
-      projects = Project.where("name LIKE ? OR place LIKE ?", name_query, place_query)
-    end
+    place_query = ''
+    place_query = params[:place] + '%' if params[:place].present?
+
+    projects = if name_query && place_query
+                Project.where('name LIKE ? AND place LIKE ?', name_query, place_query)
+              else
+                Project.where('name LIKE ? OR place LIKE ?', name_query, place_query)
+              end
     response_success(projects)
   end
 
   # POST /api/v1/projects
   def create
-    # MEMO: こちらの方がいいと思います（処理は同じです）TODO: 不要なコメントは削除して下さい
-    # TODO: ログインユーザーのIDを一緒に保存
     project = Project.create!(project_params)
     response_success(project)
   end
@@ -66,13 +51,13 @@ class Api::V1::ProjectsController < ApplicationController
       response_success(@project)
     else
       error_message = if @project.errors.full_messages_for(:name).present? && @project.errors.full_messages_for(:place).present?
-                I18n.t('response.message.both_blank')
-              elsif @project.errors.full_messages_for(:name).present?
-                I18n.t('response.message.name_blank')
-              elsif @project.errors.full_messages_for(:place).present?
-                I18n.t('response.message.place_blank')
-              end
-      render json: { error: error_message}, status: :unprocessable_entity
+                        I18n.t('response.message.both_blank')
+                      elsif @project.errors.full_messages_for(:name).present?
+                        I18n.t('response.message.name_blank')
+                      elsif @project.errors.full_messages_for(:place).present?
+                        I18n.t('response.message.place_blank')
+                      end
+      render json: { error: error_message }, status: :unprocessable_entity
     end
   end
 
