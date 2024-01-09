7755aa259737f02826e0b479da49989a85225a54
morozumisena <morozumi.sena@ring-tech.jp>
2024-01-09 16:15:42 +0900

    TODO指摘点修正

--- app/controllers/api/v1/lost_items_controller.rb
+++ app/controllers/api/v1/lost_items_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::LostItemsController < ApplicationController
   before_action :set_lost_item, only: %i[show destroy]
 
-  # POST /lost_items
-  def create
-    lost_item = LostItem.create!(lost_item_params)
-    render json: lost_item_response(lost_item), status: :created
-  end
-
-  # TODO: ここはcreateと同じ処理の中で、lost_item_imagesテーブルに保存するようにしてください
+  # POST /project_id/lost_items/id/create_with_image
   def create_with_image
     lost_item = LostItem.new(lost_item_params)
 
@@ -19,6 +13,7 @@ class Api::V1::LostItemsController < ApplicationController
     render json: lost_item_response(lost_item), status: :created
   end
 
+  # GET /project_id/lost_items
   def index
     lost_items = LostItem.all
     response_success(lost_items)
@@ -63,35 +58,12 @@ class Api::V1::LostItemsController < ApplicationController
     params.require(:lost_item_image).permit(:lost_item_id, :content)
   end
 
-  # TODO: 書き方がおかしいので修正する＆active_model_serializersを使用するようにして下さい
   def lost_item_response_destroy(lost_item)
-    { id: lost_item.id,
-      name: lost_item.name,
-      project_id: lost_item.project_id,
-      lost_storage_id: lost_item.lost_storage_id,
-      created_at: lost_item.created_at,
-      updated_at: lost_item.updated_at,
-      discarded_at: lost_item.discarded_at }
+    render json: lost_item, serializer: LostItemSerializer, only: [:id, :name, :project_id, :lost_storage_id, :created_at, :updated_at, :discarded_at]
   end
 
   def lost_item_response(lost_item)
-    response = {
-      id: lost_item.id,
-      name: lost_item.name,
-      comment: lost_item.comment,
-      features: lost_item.features,
-      owner_name: lost_item.owner_name,
-      owner_tel: lost_item.owner_tel,
-      owner_address: lost_item.owner_address,
-      lost_spot: lost_item.lost_spot,
-      project_id: lost_item.project_id,
-      lost_storage_id: lost_item.lost_storage_id,
-      created_at: lost_item.created_at,
-      updated_at: lost_item.updated_at
-    }
-
-    response[:content] = (lost_item.lost_item_images.map(&:content) if lost_item.lost_item_images.present?)
-    response
+    render json: lost_item, serializer: LostItemSerializer
   end
 
   def render_error_response(message_key, status)
---
+++ app/serializers/lost_item_serializer.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: lost_items
+#
+#  id              :bigint           not null, primary key
+#  comment         :string(255)
+#  discarded_at    :datetime
+#  features        :string(255)
+#  lost_spot       :string(255)      not null
+#  name            :string(255)      not null
+#  owner_address   :string(255)
+#  owner_name      :string(255)
+#  owner_tel       :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  lost_storage_id :bigint
+#  project_id      :bigint
+#
+# Indexes
+#
+#  index_lost_items_on_lost_storage_id  (lost_storage_id)
+#  index_lost_items_on_project_id       (project_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (lost_storage_id => lost_storages.id)
+#  fk_rails_...  (project_id => projects.id)
+#
+# == Schema Information
+#
+# Table name: lost_item_images
+#
+#  id           :bigint           not null, primary key
+#  content      :string(255)      not null
+#  discarded_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  lost_item_id :bigint
+#
+# Indexes
+#
+#  index_lost_item_images_on_lost_item_id  (lost_item_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (lost_item_id => lost_items.id)
+#
+class LostItemSerializer < ActiveModel::Serializer
+  attributes :id, :name, :comment, :features, :owner_name, :owner_tel, :owner_address, :lost_spot, :project_id, :lost_storage_id, :created_at, :updated_at, :content
+
+  def content
+    object.lost_item_images.map(&:content) if object.lost_item_images.present?
+  end
+end
