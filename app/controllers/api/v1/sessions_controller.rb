5a05c3983188159b094f11ecd018338de1b172f6
morozumisena <morozumi.sena@ring-tech.jp>
2024-01-10 19:15:29 +0900

    unless部分元に戻した

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,16 +5,19 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     # userが存在するかつ、パスワードが正しい場合
     # 参考: https://techracho.bpsinc.jp/hachi8833/2022_05_27/118000
-    raise User::NotAuthorizedError unless user&.authenticate(params[:password])
+    if user&.authenticate(params[:password])
+      data = {user_id: user.id, email: user.email}
+      # アクセストークンを作成(1日有効)
+      # TODO: ゆくゆくはJWTの有効期限を短くして、リフレッシュトークンを発行するようにして下さい
+      access_token = JWT.encode({ data: data, exp: Time.current.tomorrow.to_i }, 'secret')
+      render json: { accessToken: access_token }, status: :ok
 
-    data = { user_id: user.id, email: user.email }
-    # アクセストークンを作成(1日有効)
-    # TODO: ゆくゆくはJWTの有効期限を短くして、リフレッシュトークンを発行するようにして下さい
-    access_token = JWT.encode({ data: data, exp: Time.current.tomorrow.to_i }, 'secret')
 
-    render json: { accessToken: access_token }, status: :ok
+
     # userが存在しない場合
-
-    # 独自の例外を発生させる
+    else
+      # 独自の例外を発生させる
+      raise User::NotAuthorizedError
+    end
   end
 end
