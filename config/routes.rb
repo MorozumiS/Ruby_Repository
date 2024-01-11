db09f714b568d8859212498c1c2b14dfc72bee83
Jun <junich0911@gmail.com>
2024-01-11 21:20:25 +0900

    🎨 ルーティングの書き方を一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/auth', to: 'sessions#create'
       resources :projects do
-        get '/search' => 'projects#search' , on: :collection
+        get '/search', to: 'projects#search' , on: :collection
         resources :lost_items
+        # TODO: master確認しましたが、ここなくなっていますので追加して下さい
       end
     end
   end
